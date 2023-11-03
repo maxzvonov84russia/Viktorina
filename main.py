7efc9af383ddaeb9a3998fb3525eb9a522338003
maxzvonov84russia <146013620+maxzvonov84russia@users.noreply.github.com>
2023-11-03 18:35:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-print("Наташа мой помошник")
-print("Наташа будет помогать вам с школьной програмой")
+from PIL import Image, ImageDraw, ImageFont
+print("Генератор мемов запущен!")
+
+text_type = int(input("Введите 1, если нужен только нижний текст, и 2, если и верхний, и нижний: "))
+
+top_text = ""
+bottom_text = ""
+if text_type == 1:
+  bottom_text = input("Введите нижний текст: ")
+elif text_type == 2:
+  top_text = input("Введите верхний текст: ")
+  bottom_text = input("Введите нижний текст: ")
+else:
+  print('неправильный режим')
+  quit()
+
+print(top_text, bottom_text)
